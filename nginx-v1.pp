dc8a099f345629fd5df28673bf3a43206595bcef
Lasai <ferry.thomas@hotmail.fr>
2021-08-26 15:15:21 +0000

    first commit

---
+++ nginx-v1.pp
@@ -0,0 +1,16 @@
+package { 'nginx':
+  ensure => 'present',
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => 'true',
+}
+
+file { '/usr/share/nginx/html/index.html':
+  content => "Welcome to nginx-v1.pp By Pikachu on puppet server",
+}
+
+
+
+
